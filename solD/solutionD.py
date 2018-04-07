5e1a9988b3b3ad521c180f084fcf3bc169227a02
joseph_ctu <sephxd1234@gmail.com>
2018-04-07 19:53:07 +0200

    fully functional manual solution, prints solution

--- solD/solutionD.py
+++ solD/solutionD.py
@@ -5,13 +5,12 @@ PI = math.pi
 
 
 def solve(case_number, area):
-    print(case_number)
-    print(area)
     # center of the faces
     x_center = [0.5, 0, 0, 1]
     y_center = [0, 0.5, 0, 1]
     z_center = [0, 0, 0.5, 1]
 
+    # normal vectors
     cos_x_n = cos_N(x_center[:3])
     cos_y_n = cos_N(y_center[:3])
     cos_z_n = cos_N(z_center[:3])
@@ -25,10 +24,6 @@ def solve(case_number, area):
         y_center = matrix_mult(rot_x, y_center)
         z_center = matrix_mult(rot_x, z_center)
 
-    print(x_center[:3])
-    print(y_center[:3])
-    print(z_center[:3])
-
     cos_x_n = cos_N(x_center[:3])
     cos_y_n = cos_N(y_center[:3])
     cos_z_n = cos_N(z_center[:3])
@@ -36,27 +31,29 @@ def solve(case_number, area):
     proj_area = area_calc(cos_x_n, cos_y_n, cos_z_n)
     print('final: ' + str(proj_area))
 
-    #     # rotate
-    # b += PI/4
-    # # new area
-    # rot_x = rotX(a)
-    # rotation_matrix = matrix_mult(rot_x, x_center)
-    # x_center = rotation_matrix[:3]
-    # print(x_center)
-    return "Case #"+str(case_number + 1)+": " + str(proj_area)
+    x_string = vectorString(x_center[:3])
+    y_string = vectorString(y_center[:3])
+    z_string = vectorString(z_center[:3])
+
+    return "Case #"+str(case_number + 1)+":\n" + x_string + "\n" + y_string + "\n" + z_string
+
+
+def vectorString(vector):
+    return ' '.join(str(e) for e in vector)
 
 
 def cos_N(vector):
     normal = [0, 1, 0]
     dot_p = dotProduct(vector, normal)
-    return dot_p / (module(vector) * module(normal))
+    mod = module(vector)
+    return dot_p / mod
 
 
 def module(vector):
     result = 0
     for vect in vector:
         result += vect ** 2
-    return result
+    return math.sqrt(result)
 
 
 def dotProduct(X, Y):
