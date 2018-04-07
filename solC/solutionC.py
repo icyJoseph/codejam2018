4e43052c8f7b0ad65eeee2f7c98778a3843fdd4b
joseph_ctu <sephxd1234@gmail.com>
2018-04-07 15:25:51 +0200

    solution for small case

--- solC/solutionC.py
+++ solC/solutionC.py
@@ -1,20 +1,59 @@
 import sys
 
 
-def flusher(x, y):
-    print(x, y)
-    sys.stdout.flush()
+def check_surrounding(state, x, y):
+    # if the matrix around a point has already been done,
+    # skip it
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            x_test = x + i
+            y_test = y + j
+            if (x_test, y_test) not in state:
+                return True
+    return False
 
 
-def decision(i, j):
-    return i + 10, j-10
-
-
-grid = [1000, 1000]
 number_of_tests = int(input())
+
 for test in range(0, number_of_tests):
+    done = False
     area = int(input())
-    flusher(10, 10)
-    i, j = [int(elem) for elem in input()]
-    x, y = decision(i, j)
-    flusher(x, y)
+    path = 0
+    if area < 100:
+        path = area
+        folds = area // 2
+    else:
+        folds = (area // 1000) + 1
+        path = 1000
+    direction = 'y'
+    x, y = 2, 2
+    state = set()
+    while not done:
+        # check whether or not to give the command:
+        if check_surrounding(state, x, y):
+            # give a command to gopher
+            print(x, y)
+            sys.stdout.flush()
+            # response from gopher
+            response = input().split()
+            i, j = [int(elem) for elem in response]
+            state.add((i, j))
+            # terminate?
+            if [i, j] == [-1, -1]:
+                exit()
+            elif [i, j] == [0, 0]:
+                done = True
+        else:
+            if direction == 'y':
+                y += 1
+                if y >= path + 1:
+                    direction = 'x'
+            elif direction == 'x':
+                x += 1
+                y = 2
+                direction = 'y'
+                if x >= folds:
+                    y = 2
+                    x = 2
+                    direction = 'y'
+exit()
