d3a21aa2151dad9331fd7d98e96c4e37a7189549
joseph_ctu <sephxd1234@gmail.com>
2018-04-07 07:44:36 +0200

    solution A: parse input and process instructions

--- solA/solutionA.py
+++ solA/solutionA.py
@@ -20,7 +20,45 @@ def print_output(filename, list):
 
 # Solution goes here
 def process(problem):
-    return ['dummy', 'dummy2']
+    number_of_cases = int(problem[0][0])
+    cases = problem[1:]
+    solution = [solve(case_number, cases[case_number])
+                for case_number in range(0, number_of_cases)]
+    return solution
+
+
+def solve(case_number, case):
+    shield_string, instructions = case
+    shield = int(shield_string)
+    print(case_number)
+    x = robot_wins(instructions, shield)
+    print(x)
+    return "Case #"+str(case_number + 1)+": " + "SOLUTION"
+
+
+def robot_wins(instructions, shield):
+    instructions_arr = [instruction for instruction in instructions]
+    damage = 1
+    for command in instructions_arr:
+        if command == 'S':
+            print('shoot with: ' + str(damage) + " to shield: " + str(shield))
+            if (damage > shield):
+                return True
+        if command == 'C':
+            damage += damage
+    return False
+
+
+def hack_robot(instructions_arr, left):
+    # swap S's
+    for index in range(0, instructions_arr):
+        command = instructions_arr[index]
+        if command == 'S':
+            next_command = instructions_arr[index+1]
+            if next_command == 'C':
+                instructions_arr[index] = 'C'
+                instructions_arr[index+1] = 'S'
+                return instructions_arr
 
 
 filename = "example.in"
